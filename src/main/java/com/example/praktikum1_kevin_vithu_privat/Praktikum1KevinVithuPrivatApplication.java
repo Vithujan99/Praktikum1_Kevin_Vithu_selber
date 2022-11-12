9825a36c0eb67250f31a6cc3ff682f700dfe276f
Vithujan99 <vithujan99@hotmail.com>
2022-11-12 15:28:20 +0100

    Git austesten

---
+++ src/main/java/com/example/praktikum1_kevin_vithu_privat/Praktikum1KevinVithuPrivatApplication.java
@@ -0,0 +1,13 @@
+package com.example.praktikum1_kevin_vithu_privat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Praktikum1KevinVithuPrivatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Praktikum1KevinVithuPrivatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/praktikum1_kevin_vithu_privat/Praktikum1KevinVithuPrivatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.praktikum1_kevin_vithu_privat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Praktikum1KevinVithuPrivatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
